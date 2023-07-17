f3a1f68f66c4941d365a2175d5541883da0334ac
MuhammedYahiya <muhammedyahiyah@gmail.com>
2023-07-18 01:22:59 +0530

    using statefull widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
 
 const startAligment = Alignment.topLeft;
 const endAligment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  GradientContainer.purple({super.key})
+  const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1, color2;
-
-  var activeDiceImage = 'assets/images/dice-2.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print('Changing image...');
-  }
 
   @override
   Widget build(context) {
@@ -29,30 +23,8 @@ class GradientContainer extends StatelessWidget {
           end: endAligment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(
-                //   top: 20,
-                // ),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(
-          const Color.fromRGBO(126, 94, 216, 1),
-          const Color.fromARGB(255, 23, 3, 79),
+          Color.fromRGBO(126, 94, 216, 1),
+          Color.fromARGB(255, 23, 3, 79),
         ),
       ),
     ),
