d5cc3cbe599b487f7edf1da1524cef0886573f76
MuhammedYahiya <muhammedyahiyah@gmail.com>
2023-07-19 21:32:48 +0530

    remove water mark from app

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:first_app/gradient_container.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GradientContainer(
           Color.fromRGBO(126, 94, 216, 1),
